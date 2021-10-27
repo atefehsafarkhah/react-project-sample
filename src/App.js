afb2cb4c617f21485d22ae5176ce1476a89c781c
atefehsafarkhah <atefeh.safarkhah@yahoo.com>
2021-10-27 17:53:52 +0200

    component-part2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      dishes: DISHES
+    };
+  }
+
+
+  render() {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,9 +22,9 @@ function App() {
           <NavbarBrand herf="/"> Ristorante Con Fusion </NavbarBrand>
         </div>
        </Navbar> 
-       <Menu />
+       <Menu dishes={this.state.dishes} />
     </div>
   );
 }
-
+}
 export default App;
